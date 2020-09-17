b301e732d3ec5025776693ac78365793b6964e8c
abbasspark <abbasbandar@gmail.com>
2020-09-17 10:46:35 -0300

    vvv

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,5 @@ app.use("/search", require("./search"));
 app.use("/subtitles", require("./subtitles"));
 app.use("/yts", require("./yts"));
 app.use("/popapi", require("./popcorn"));
+app.use("/poptorrent", require("./popTorrent"));
 module.exports = app;
